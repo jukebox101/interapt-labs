30df9576b1e6e1f9b4d1ccf208363680681a4543
jukebox101 <leonora.squires@gmail.com>
2023-09-07 10:47:11 -0400

    part 2 of flow control completed

--- Ch5-FlowControl/flow-control.py
+++ Ch5-FlowControl/flow-control.py
@@ -1,5 +1,6 @@
 import sys;
 
+# Part 1
 target = 45
 guesses = 5
 start_answer = input("Wanna play a number game? (y or n) ==> ")
@@ -12,7 +13,24 @@ if start_answer == 'y':
             sys.exit(0)
         else:
             guesses -= 1
-            print('No dice, try again')        
+            print(f'No dice, try again! You have {guesses} guesses left!')        
             user_guess = input('Enter a number between 1 and 100 ==> ')
 elif start_answer == 'n':
     sys.exit(0)
+
+# Part 2
+user_input = input('Enter:/n    C to create,/n  R to read,/n    U to update,/n  D to delete or/n    Q to quit ==> ') 
+while user_input.upper() != 'Q' :
+    if user_input.upper() == 'C':
+        print("Calling CREATE routine...")
+        user_input = input('Enter:/n    C to create,/n  R to read,/n    U to update,/n  D to delete or/n    Q to quit ==> ')
+    elif user_input.upper() == 'R':
+        print("Calling READ routine...")
+        user_input = input('Enter:/n    C to create,/n  R to read,/n    U to update,/n  D to delete or/n    Q to quit ==> ')
+    elif user_input.upper() == 'U':
+        print("Calling UPDATE routine...")
+        user_input = input('Enter:/n    C to create,/n  R to read,/n    U to update,/n  D to delete or/n    Q to quit ==> ')
+    elif user_input.upper() == 'D':
+        print("Calling DELETE routine...")
+        user_input = input('Enter:/n    C to create,/n  R to read,/n    U to update,/n  D to delete or/n    Q to quit ==> ')
+print("Exiting the program...")
