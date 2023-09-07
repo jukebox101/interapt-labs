241e4b3ecf7129d0604ff42b012e6690a0a796d1
jukebox101 <leonora.squires@gmail.com>
2023-09-07 10:49:30 -0400

    constant added

--- Ch5-FlowControl/flow-control.py
+++ Ch5-FlowControl/flow-control.py
@@ -1,14 +1,14 @@
 import sys;
 
 # Part 1
-target = 45
+TARGET = 45
 guesses = 5
 start_answer = input("Wanna play a number game? (y or n) ==> ")
 
 if start_answer == 'y':
     user_guess = input('Enter a number between 1 and 100 ==> ')
     for guess in range(guesses):    
-        if int(user_guess) == target:
+        if int(user_guess) == TARGET:
             print('Great job! You guessed correctly')
             sys.exit(0)
         else:
