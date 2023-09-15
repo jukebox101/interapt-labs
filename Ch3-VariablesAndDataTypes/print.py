4014b6957a3d9580b0ed273667658fb7f40f14a8
jukebox101 <leonora.squires@gmail.com>
2023-09-15 12:58:51 -0400

    variables scrap updated

--- Ch3-VariablesAndDataTypes/print.py
+++ Ch3-VariablesAndDataTypes/print.py
@@ -4,11 +4,3 @@
 # print(aVar + bVar)
 # print(f'first variable: {aVar}, second variable: {bVar}')
 
-letter_a = "A"
-three_spaces = "   "
-print(f'{letter_a: ^38}')
-print(f'{letter_a: >17}{three_spaces}{letter_a:<17}')
-print(f'{letter_a: >16}',end="" )
-print(f'{letter_a}{letter_a}{letter_a}{letter_a}{letter_a}{letter_a:<15}' )
-print(f'{letter_a: >15}{three_spaces:>7}{letter_a:<15}')
-print(f'{letter_a: >14}{three_spaces:>9}{letter_a:<14}')
