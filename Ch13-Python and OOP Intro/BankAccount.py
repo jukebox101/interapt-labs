e3d383e132d1a882e9bfae1bf99a2b1920266596
jukebox101 <leonora.squires@gmail.com>
2023-09-20 14:35:26 -0400

    class bank account lab updated

--- Ch13-Python and OOP Intro/BankAccount.py
+++ Ch13-Python and OOP Intro/BankAccount.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 class BankAccount:
-    def __init__(self, acctID, acct_holder, date_opened, balance):
+    def __init__(self, acctID, acct_holder, balance):
         self._acctID = acctID
         self._acct_holder = acct_holder
-        self._date_opened = date_opened
+        self._date_opened = datetime.now()
         self._balance = balance
 
     @property
@@ -22,12 +24,22 @@ class BankAccount:
         else:
             self._balance = self._balance + deposit_amount
 
-    def make_withdrawl(self, withdrawal_amount):
+    def make_withdrawal(self, withdrawal_amount):
         if withdrawal_amount < 0:
             print(f'Withdrawl amount must exceed 0. Withdrawl amount was: {withdrawal_amount}')
         elif self._balance - withdrawal_amount < 0:
             print(f'Withdrawl of {withdrawal_amount} failed. Overdrafts are not allowed.')
         else:
             self._balance = self._balance - withdrawal_amount
+
+    def transfer(self, transfer_amount, target_acctID):
+        if self._balance - transfer_amount > 0 and self._acct_holder == target_acctID._acct_holder:
+            target_acctID._balance += transfer_amount
+            self._balance -= transfer_amount
+        else:
+            print(f'Sorry you are unable to transfer funds')
+    
+    def print_acct_info(self):
+        print(f' Account ID: {self._acctID}\n Account Holder: {self._acct_holder}\n Date opened: {self._date_opened}\n Account Balance: {self._balance}')
 
     
