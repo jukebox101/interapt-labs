45eb4b6d6a0de8da2c50f0e5a99ea7313b1c302d
jukebox101 <leonora.squires@gmail.com>
2023-09-20 11:55:28 -0400

    president lab added

--- president object lab/president.py
+++ president object lab/president.py
@@ -1,76 +1,60 @@
-from datetime import date
-
-
-class President():
-    def __init__(self, index):
-        self._get_data(index)
-
-    @staticmethod
-    def _mkdate(raw_date):
-        if raw_date != "NONE":
-            raw_year, raw_month, raw_day = raw_date.split('-')
-            d = date(int(raw_year), int(raw_month), int(raw_day))
-        else:
-            d = None
-
-        return d
-
-    def _get_data(self, index):
-        with open("../DATA/presidents.txt") as pfile:
-            for line in pfile:
-                flds = line.rstrip().split(":")
-                if int(flds[0]) == int(index):
-                    self._lname = flds[1]
-
-                    self._fname = flds[2]
-
-                    self._bdate = self._mkdate(flds[3])
-                    self._ddate = self._mkdate(flds[4])
-
-                    self._bplace = flds[5]
-                    self._bstate = flds[6]
-
-                    self._ts_date = self._mkdate(flds[7])
-                    self._te_date = self._mkdate(flds[8])
-
-                    self._party = flds[9]
+class President:
+    def __init__(self, term_num):
+        with open("presidents.txt") as file:
+            for line in file:
+                field_list = line.split(":")
+                if term_num == int(field_list[0]):
+                    self._term_num = term_num
+                    self._last_name = field_list[1]
+                    self._first_name = field_list[2]
+                    self._birth_date = field_list[3]
+                    self._death_date = field_list[4]
+                    self._birth_place = field_list[5]
+                    self._birth_state = field_list[6]
+                    self._term_start_date = field_list[7]
+                    self._term_end_date = field_list[8]
+                    self._party = field_list[9]
 
                     break
-            else:
-                raise ValueError("Invalid term number")
-
+                else:
+                    raise ValueError(f"Invalid term number '{term_num}' passed")
+    @property
+    def term_num(self):
+        return self._term_num
+    
     @property
     def last_name(self):
-        return self._lname
-
+        return self._last_name   
+     
     @property
     def first_name(self):
-        return self._fname
-
+        return self._first_name   
+     
     @property
     def birth_date(self):
-        return self._bdate
-
+        return self._birth_date    
+    
     @property
     def death_date(self):
-        return self._ddate
-
+        return self._death_date    
+    
     @property
     def birth_place(self):
-        return self._bplace
-
+        return self._birth_place    
+    
     @property
     def birth_state(self):
-        return self._bstate
-
+        return self._birth_state   
+    
     @property
     def term_start_date(self):
-        return self._ts_date
-
+        return self._term_start_date    
+    
     @property
     def term_end_date(self):
-        return self._te_date
-
+        return self._term_end_date
+    
     @property
     def party(self):
         return self._party
+    
